9712093f22ab601d93adcdcf27ecafe543de198d
Dana-M01 <dsmorenoa01@libertadores.edu.co>
2025-05-22 02:24:13 +0000

    Fondo

--- script.js
+++ script.js
@@ -1,6 +1,47 @@
 // Inicialización del controlador de ScrollMagic
 const controller = new ScrollMagic.Controller();
 
+// Animación de las nubes
+gsap.to('.cloud-1', {
+    x: '+=50',
+    duration: 20,
+    repeat: -1,
+    yoyo: true,
+    ease: 'none'
+});
+
+gsap.to('.cloud-2', {
+    x: '-=40',
+    duration: 15,
+    repeat: -1,
+    yoyo: true,
+    ease: 'none'
+});
+
+gsap.to('.cloud-3', {
+    x: '+=45',
+    duration: 18,
+    repeat: -1,
+    yoyo: true,
+    ease: 'none'
+});
+
+gsap.to('.cloud-4', {
+    x: '-=35',
+    duration: 22,
+    repeat: -1,
+    yoyo: true,
+    ease: 'none'
+});
+
+gsap.to('.cloud-5', {
+    x: '+=40',
+    duration: 25,
+    repeat: -1,
+    yoyo: true,
+    ease: 'none'
+});
+
 // Animación del título
 const titleTimeline = gsap.timeline()
     .from('.title', {
